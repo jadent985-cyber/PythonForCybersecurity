f6920efce6a6891633dcd135e421ac80348580ac
Jaden Thompson <jadent985@gmail.com>
2026-08-02 23:36:07 -0700

    conditional update

--- end/CH03/Jaden_Thompson_Assigment2.py
+++ end/CH03/Jaden_Thompson_Assigment2.py
@@ -1,5 +1,11 @@
-good_day = input("Is today a good day? (y/n)")
-yes = "y"
-if yes == "y":
+def send_message():
     for yes in range(10):
          print ("Yes it is")
+
+
+
+good_day = input("Is today a good day? (y/n)")
+ 
+if good_day == "y":
+    send_message()    
+    
