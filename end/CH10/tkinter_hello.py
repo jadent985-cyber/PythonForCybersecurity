f01e5303037ea779365600eb88ee99fe8b2dc103
Jaden Thompson <jadent985@gmail.com>
2026-08-26 19:47:36 -0700

    GUI

--- end/CH10/tkinter_hello.py
+++ end/CH10/tkinter_hello.py
@@ -8,9 +8,18 @@ import tkinter
 my_window = tkinter.Tk()
 
 # Add widgets
-my_label = tkinter.Label(my_window, text = "Hello World",
-                         font = ("Arial Bold", 50))
-my_label.pack()
+tkinter.Label(my_window, text="Enter your name:").pack()
+
+name_entry= tkinter.Entry(my_window)
+name_entry.pack()
+
 
 # Enter the main event loop
+def hello():
+    name = name_entry.get()
+    label.config(text="Hello" + name + "\nToday is going to be a great day!")
+tkinter.Button(my_window, text="Submit", command=hello).pack()
+label = tkinter.Label(my_window, text="")
+label.pack()
+
 my_window.mainloop()
